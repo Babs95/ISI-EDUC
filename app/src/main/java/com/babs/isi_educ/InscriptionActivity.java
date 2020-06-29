af8c950a115e1831dc933d66f331cd876a22b4b6
Babs95 <babsco95@gmail.com>
2020-06-29 18:48:50 +0000

    Ajout http

--- app/src/main/java/com/babs/isi_educ/InscriptionActivity.java
+++ app/src/main/java/com/babs/isi_educ/InscriptionActivity.java
@@ -2,12 +2,25 @@ package com.babs.isi_educ;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class InscriptionActivity extends AppCompatActivity {
     private EditText txtFirstName, txtLastName;
@@ -43,9 +56,11 @@ public class InscriptionActivity extends AppCompatActivity {
                 if (cbMaster.isChecked()) {
                     degrees+= cbMaster.getText().toString().trim()+"";
                 }
-                String resume = firstname+"\n\n"+lastname+"\n\n"+degrees+"\n\n"+formation;
+                inscription();
+                //String resume = firstname+"\n\n"+lastname+"\n\n"+degrees+"\n\n"+formation;
                 EraseFields();
-                Toast.makeText(InscriptionActivity.this, resume, Toast.LENGTH_SHORT).show();
+
+                //Toast.makeText(InscriptionActivity.this, resume, Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -69,5 +84,68 @@ public class InscriptionActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    public void inscription() {
+        try {
+            String url ="http://192.168.1.104/isi/inscription.php";
+            OkHttpClient client = new OkHttpClient();
+            RequestBody body = new FormBody.Builder()
+                    .add("first_name", firstname)
+                    .add("last_name", lastname)
+                    .add("degrees", degrees)
+                    .add("formation", formation)
+                    .build();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, final IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String error = getString(R.string.error_connection);
+                            Toast.makeText(InscriptionActivity.this, error, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        JSONObject jo = new JSONObject(response.body().string());
+                        String status = jo.getString("status");
+
+                        if(status.equalsIgnoreCase("KO")) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String message =getString(R.string.error_parameters);
+                                    Toast.makeText(InscriptionActivity.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String message =getString(R.string.success_save);
+                                    Toast.makeText(InscriptionActivity.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+
+                    }catch (Exception e){
+
+                    }
+                }
+            });
+
+        }catch (Exception e){
+
+        }
     }
 }
